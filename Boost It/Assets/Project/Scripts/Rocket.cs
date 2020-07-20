2663fa616b5fde4f4182ed72b8feb62aecfd7793
viniaf13 <viniaf13@gmail.com>
2020-07-20 19:32:44 -0300

    adjusted thrust logic

--- Boost It/Assets/Project/Scripts/Rocket.cs
+++ Boost It/Assets/Project/Scripts/Rocket.cs
@@ -7,16 +7,18 @@ using UnityEngine.InputSystem;
 public class Rocket : MonoBehaviour
 {
     [SerializeField] float rotationSensitivity = 100f;
-    [SerializeField] float thrustForce = 20f;
+    [SerializeField] float thrustForce = 1f;
 
     private PlayerControls controls;
+    private bool isThrusting;
     private float direction;
     private Rigidbody rigidBody;
 
     void Awake()
     {
         controls = new PlayerControls();
-        controls.Gameplay.Thrust.performed += _ => Thrust();
+        controls.Gameplay.Thrust.performed += _ => isThrusting = true;
+        controls.Gameplay.Thrust.canceled += _ => isThrusting = false;
 
         controls.Gameplay.Rotate.performed += ctx => direction = ctx.ReadValue<float>();
         controls.Gameplay.Rotate.canceled += ctx => direction = 0f;
@@ -28,16 +30,18 @@ public class Rocket : MonoBehaviour
     }
     private void Update()
     {
+        Thrust();
         Rotate();
     }
     private void Thrust()
     {
+        if (!isThrusting) return;
         rigidBody.AddRelativeForce(Vector3.up * thrustForce);
     }
     private void Rotate()
     {
-        Vector3 rotation = new Vector3(0, 0, rotationSensitivity * direction) * Time.deltaTime;
-        transform.Rotate(rotation);
+        Vector3 rotationVector = Vector3.forward * rotationSensitivity * direction;
+        transform.Rotate(rotationVector * Time.deltaTime) ;
     }
 
     private void OnEnable()
