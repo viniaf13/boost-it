d11f8e5ff406255fca6d17ea2c7ac479f102efeb
viniaf13 <viniaf13@gmail.com>
2020-07-20 16:19:11 -0300

    imported new input system

---
+++ Boost It/Assets/Project/Scripts/Rocket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        
+    }
+}
